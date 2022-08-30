0c60e2646a63341dab86c77acab243005c780221
William Dupont <williamd.contact@gmail.com>
2022-08-30 15:22:16 +0100

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by William Dupont on 30/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
